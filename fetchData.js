b8b2a240257f5694cf66922109b08234097632c3
Chris M <christophertmartin1993@gmail.com>
2018-04-21 20:21:08 +0100

    getting set up

--- fetchData.js
+++ fetchData.js
@@ -2,12 +2,10 @@ import https from 'https';
 
 const getData = () => (
     https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-          let data = '';
-        
-          // A chunk of data has been recieved.
-          resp.on('data', (chunk) => {
-            data += chunk;
-          });
+        let data;
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
     })
 );
 
